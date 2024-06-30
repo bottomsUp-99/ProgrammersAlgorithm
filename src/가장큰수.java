b750e7f5c2bba3ecde1fdde7c765d60382c39086
Lee JuHwan <seungjoon0324@gmail.com>
2024-06-30 21:16:29 +0900

    프로그래머스 가장큰수

---
+++ src/가장큰수.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class 가장큰수 {
+
+  public static void main(String[] args) {
+    int[] numbers = {6, 10, 2};
+    System.out.println(solution(numbers));
+  }
+
+  public static String solution(int[] numbers) {
+    StringBuilder answer = new StringBuilder();
+    String[] tmp = new String[numbers.length];
+
+    for (int i = 0; i < tmp.length; i++) {
+      tmp[i] = String.valueOf(numbers[i]);
+    }
+    Arrays.sort(tmp, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+    for (String s : tmp) {
+      answer.append(s);
+    }
+    return String.valueOf(answer);
+  }
+}
