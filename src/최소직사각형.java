8cd4ba4eab744200b5a99ecb1732d3b64930297f
Lee JuHwan <seungjoon0324@gmail.com>
2024-07-18 09:16:01 +0900

    최소직사각형 완

--- src/최소직사각형.java
+++ src/최소직사각형.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class 최소직사각형 {
 
   public static void main(String[] args) {
@@ -8,38 +10,17 @@ public class 최소직사각형 {
     System.out.println(solution(size1));
   }
   public static int solution(int[][] sizes){
-    int x = getMaxX(sizes);
-    int y = getMaxY(sizes);
-    return findMinRectangle(sizes,x*y);
-  }
-  public static int findMinRectangle(int[][] sizes, int compareArea){
-    int count = 0;
-    while (count < sizes.length){
-      for (int i = 0; i < sizes.length; i++) {
-        int[][] compareArr = sizes;
-        int tmp = compareArr[i][0];
-        compareArr[i][0] = compareArr[i][1];
-        compareArr[i][1] = tmp;
-        if (compareArea > getMaxY(compareArr)*getMaxY(compareArr)) {
-          compareArea = getMaxY(compareArr)*getMaxY(compareArr);
-        }
-      }
-      count++;
-    }
-    return compareArea;
-  }
-  public static int getMaxX(int[][] sizes){
-    int x_max = 0;
+    int max_x = 0;
+    int max_y = 0;
+
     for (int i = 0; i < sizes.length; i++) {
-      if (sizes[i][0] > x_max) x_max = sizes[i][0];
+      Arrays.sort(sizes[i]);
     }
-    return x_max;
-  }
-  public static int getMaxY(int[][] sizes){
-    int y_max = 0;
     for (int i = 0; i < sizes.length; i++) {
-      if (sizes[i][0] > y_max) y_max = sizes[i][0];
+        max_x = Math.max(max_x, sizes[i][0]);
+        max_y = Math.max(max_y, sizes[i][1]);
     }
-    return y_max;
+
+    return max_x * max_y;
   }
 }
